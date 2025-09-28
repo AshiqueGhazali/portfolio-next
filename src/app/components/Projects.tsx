900d732ba9f5532f8afd02fc5ce2354555b80494
AshiqueGhazali <155415365+AshiqueGhazali@users.noreply.github.com>
2025-09-28 14:05:46 +0530

    update: project tech stack animation added

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import projectsData from "../utils/projectData";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
@@ -30,24 +31,34 @@ const Projects = () => {
             <p className="text-[14px] font-normal pointer-events-none text-gray-400 mt-2">
               {item.details}
             </p>
-            {/* <div className="flex lg:hidden flex-wrap gap-2 mt-3 transition-all duration-300">
-              {item.techstack.map((tech, techIndex) => (
-                <span
-                  key={techIndex}
-                  className="text-[12px] bg-[#17264b] text-gray-400 px-2 py-1 rounded-[3px]"
-                >
-                  {tech}
-                </span>
-              ))}
-            </div> */}
             <div className="overflow-hidden mt-3">
-              <div className="flex flex-wrap gap-2 transform translate-y-[-100%] opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500 ease-out">
+              <div className="flex flex-wrap gap-2 lg:hidden">
+                {item.techstack.map((tech, techIndex) => (
+                  <motion.span
+                    key={techIndex}
+                    className="text-[12px] bg-[#17264b] text-gray-400 px-2 py-1 rounded-[3px]"
+                    initial={{ opacity: 0, y: -20 }} // Start above & invisible
+                    whileInView={{ opacity: 1, y: 0 }} // Animate to visible & original position
+                    viewport={{ once: true, amount: 0.2 }} // Trigger once, when 20% visible
+                    transition={{
+                      duration: 0.3,
+                      delay: techIndex * 0.1,
+                      ease: "easeOut",
+                    }}
+                  >
+                    {tech}
+                  </motion.span>
+                ))}
+              </div>
+            </div>
+            <div className="overflow-hidden mt-3">
+              <div className="hidden lg:flex flex-wrap gap-2 transform translate-y-[-100%] opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500 ease-out">
                 {item.techstack.map((tech, techIndex) => (
                   <span
                     key={techIndex}
                     className="text-[12px] bg-[#17264b] text-gray-400 px-2 py-1 rounded-[3px] transform translate-y-2 group-hover:translate-y-0 transition-transform duration-300 ease-out"
-                    style={{ 
-                      transitionDelay: `${techIndex * 50}ms` 
+                    style={{
+                      transitionDelay: `${techIndex * 50}ms`,
                     }}
                   >
                     {tech}
